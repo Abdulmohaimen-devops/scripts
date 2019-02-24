702994337fc33375bf105d1e4b0de6a7db1f2ce3
Abdulmohaimen <46815218+Abdulmohaimen-devops@users.noreply.github.com>
2019-02-25 01:57:04 +0200

    Create test_space.sh
    this script for send email when disk space exceeded to specific value(90)

---
+++ test_space.sh
@@ -0,0 +1,13 @@
+#this script for send email when disk space exceeded to specific value(90)
+
+#!/bin/sh
+df -H | grep -vE '^Filesystem|tmpfs|cdrom|udev' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge 90 ]; then
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
+     mail -s "Alert: Almost out of disk space $usep%" yourmail@domain.com 
+  fi
+done
